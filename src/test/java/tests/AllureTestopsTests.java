b45a3d23e3693232c4502a731fc538eacf3d82e4
AloeLavi <story_teller@bk.ru>
2022-12-10 23:43:05 +0300

    added steps test

--- src/test/java/tests/AllureTestopsTests.java
+++ src/test/java/tests/AllureTestopsTests.java
@@ -4,18 +4,20 @@ import api.AuthorizationApi;
 import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
+import models.lombok.CreateStepBody;
 import models.lombok.CreateTestCaseBody;
+import models.lombok.Step;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
 
+import java.util.List;
+
 import static api.AuthorizationApi.ALLURE_TESTOPS_SESSION;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static helpers.CustomApiListener.withCustomTemplates;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.CoreMatchers.is;
@@ -36,107 +38,9 @@ public class AllureTestopsTests {
     }
 
     @Test
-    void loginTest() {
-        open("");
+    void createTestSteps() {
 
-        $(byName("username")).setValue(USERNAME);
-        $(byName("password")).setValue(PASSWORD).pressEnter();
-
-        $("button[aria-label=\"User menu\"]").click();
-        $(".Menu__item_info").shouldHave(text(USERNAME));
-    }
-
-    @Test
-    void loginWithApiSimpleTest() {
-        String xsrfToken = given()
-                .formParam("grant_type", "apitoken")
-                .formParam("scope", "openid")
-                .formParam("token", "efd32a69-217f-41fa-9701-55f54dd55cd4")
-                .when()
-                .post("/api/uaa/oauth/token")
-                .then()
-                .statusCode(200)
-                .extract()
-                .path("jti");
-
-        String authorizationCookie = given()
-                .header("X-XSRF-TOKEN", xsrfToken)
-                .header("Cookie", "XSRF-TOKEN=" + xsrfToken)
-                .formParam("username", USERNAME)
-                .formParam("password", PASSWORD)
-                .when()
-                .post("/api/login/system")
-                .then()
-                .statusCode(200).extract().response()
-                .getCookie(ALLURE_TESTOPS_SESSION);
-
-        open("/favicon.ico");
-        getWebDriver().manage().addCookie(new Cookie(ALLURE_TESTOPS_SESSION, authorizationCookie));
-
-        open("");
-        $("button[aria-label=\"User menu\"]").click();
-        $(".Menu__item_info").shouldHave(text(USERNAME));
-    }
-
-    @Test
-    void loginWithApiTest() {
-        String authorizationCookie = new AuthorizationApi()
-                .getAuthorizationCookie(USER_TOKEN, USERNAME, PASSWORD);
-
-        open("/favicon.ico");
-        getWebDriver().manage().addCookie(new Cookie(ALLURE_TESTOPS_SESSION, authorizationCookie));
-
-        open("");
-        $("button[aria-label=\"User menu\"]").click();
-        $(".Menu__item_info").shouldHave(text(USERNAME));
-    }
-
-    @Test
-    void viewTestCaseWithApiTest() {
-        /*
-        1. Make GET request.ftl to /api/rs/testcase/13328/overview
-        2. Check name is "View test case name"
-     */
-        String authorizationCookie = new AuthorizationApi()
-                .getAuthorizationCookie(USER_TOKEN, USERNAME, PASSWORD);
-
-        given()
-                .log().all()
-                .cookie(ALLURE_TESTOPS_SESSION, authorizationCookie)
-                .get("/api/rs/testcase/13328/overview")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .body("name", is("View test case name"));
-    }
-
-    @Test
-    void viewTestCaseWithUiTest() {
-        /*
-        1. Open page /project/1722/test-cases/13328
-        2. Check name is "View test case name"
-     */
-        String authorizationCookie = new AuthorizationApi()
-                .getAuthorizationCookie(USER_TOKEN, USERNAME, PASSWORD);
-
-        open("/favicon.ico");
-        getWebDriver().manage().addCookie(new Cookie(ALLURE_TESTOPS_SESSION, authorizationCookie));
-
-        open("/project/1722/test-cases/13328");
-        $(".TestCaseLayout__name").shouldHave(text("View test case name"));
-    }
-
-    @Test
-    void createTestCaseWithApiTest() {
-        /*
-        1. Make POST request.ftl to /api/rs/testcasetree/leaf?projectId=1722
-                with body {"name":"Some random test"}
-        2. Get test case {id} from response.ftl {"id":13330,"name":"Some random test","automated":false,"external":false,"createdDate":1669920611154,"statusName":"Draft","statusColor":"#abb8c3"}
-        3. Open page /project/1722/test-cases/{id}
-        4. Check name is "Some random test"
-     */
         AuthorizationApi authorizationApi = new AuthorizationApi();
-
         String xsrfToken = authorizationApi.getXsrfToken(USER_TOKEN);
         String authorizationCookie = authorizationApi
                 .getAuthorizationCookie(USER_TOKEN, xsrfToken, USERNAME, PASSWORD);
@@ -148,8 +52,16 @@ public class AllureTestopsTests {
         testCaseBody.setName(testCaseName);
 //        String testCaseBody = "{\"name\":\"Some random test\"}";
 
+        Step FirstStep = new Step();
+        FirstStep.setName("Шажочек1");
+        Step SecondStep = new Step();
+        SecondStep.setName("Шажочек2");
+
+        CreateStepBody StepBody = new CreateStepBody();
+        StepBody.setSteps(List.of(FirstStep, SecondStep));
+
         int testCaseId = given()
-                .log().all()
+           //     .log().all()
                 .header("X-XSRF-TOKEN", xsrfToken)
                 .cookies("XSRF-TOKEN", xsrfToken,
                         ALLURE_TESTOPS_SESSION, authorizationCookie)
@@ -157,94 +69,36 @@ public class AllureTestopsTests {
                 .contentType(JSON)
                 .queryParam("projectId", "1722")
                 .post("/api/rs/testcasetree/leaf")
-//                .post("/api/rs/testcasetree/leaf?projectId=1722")
                 .then()
-                .log().body()
+            //    .log().body()
                 .statusCode(200)
                 .body("name", is(testCaseName))
                 .body("automated", is(false))
                 .body("external", is(false))
                 .extract()
                 .path("id");
-
-        open("/favicon.ico");
-        getWebDriver().manage().addCookie(new Cookie(ALLURE_TESTOPS_SESSION, authorizationCookie));
-        open("/project/1722/test-cases/" + testCaseId);
-        $(".TestCaseLayout__name").shouldHave(text(testCaseName));
-    }
-
-    @Test
-    void createTestCaseStepsTest() {
-        /*
-        1. Make POST request.ftl to /api/rs/testcasetree/leaf?projectId=1722
-                with body {"name":"Some random test"}
-        2. Get test case {id} from response.ftl {"id":13330,"name":"Some random test","automated":false,"external":false,"createdDate":1669920611154,"statusName":"Draft","statusColor":"#abb8c3"}
-        3. Open page /project/1722/test-cases/{id}
-        4. Check name is "Some random test"
-     */
-        AuthorizationApi authorizationApi = new AuthorizationApi();
-
-        String xsrfToken = authorizationApi.getXsrfToken(USER_TOKEN);
-        String authorizationCookie = authorizationApi
-                .getAuthorizationCookie(USER_TOKEN, xsrfToken, USERNAME, PASSWORD);
-
-        Faker faker = new Faker();
-        String testCaseName = faker.name().nameWithMiddle();
-
-        CreateTestCaseBody testCaseBody = new CreateTestCaseBody();
-        testCaseBody.setName(testCaseName);
-//        String testCaseBody = "{\"name\":\"Some random test\"}";
-
-        int testCaseId = given()
-                .log().all()
-                .header("X-XSRF-TOKEN", xsrfToken)
-                .cookies("XSRF-TOKEN", xsrfToken,
-                        ALLURE_TESTOPS_SESSION, authorizationCookie)
-                .body(testCaseBody)
-                .contentType(JSON)
-                .queryParam("projectId", "1722")
-                .post("/api/rs/testcasetree/leaf")
-//                .post("/api/rs/testcasetree/leaf?projectId=1722")
-                .then()
-                .log().body()
-                .statusCode(200)
-                .body("name", is(testCaseName))
-                .body("automated", is(false))
-                .body("external", is(false))
-                .extract()
-                .path("id");
-
-        // создаем шаг
-
-        String steps = {"steps": [{"name": "Первый шаг", "steps": [], "leaf": true,
-                "stepsCount": 0,
-                "hasContent": false
-        }
-  ]
-}
 
         given()
                 .log().all()
                 .header("X-XSRF-TOKEN", xsrfToken)
                 .cookies("XSRF-TOKEN", xsrfToken,
                         ALLURE_TESTOPS_SESSION, authorizationCookie)
-                .body(testCaseBody)
+                .body(StepBody)
                 .contentType(JSON)
                 .queryParam("projectId", "1722")
-                .post("/api/rs/testcasetree/leaf")
-//                .post("/api/rs/testcasetree/leaf?projectId=1722")
+                .post("/api/rs/testcase/" + testCaseId + "/scenario")
                 .then()
-                .log().body()
+                .log().all()
                 .statusCode(200)
-                .body("name", is(testCaseName))
-                .body("automated", is(false))
-                .body("external", is(false))
-                .extract()
-                .path("id");
+               ;
 
         open("/favicon.ico");
         getWebDriver().manage().addCookie(new Cookie(ALLURE_TESTOPS_SESSION, authorizationCookie));
-        open("/project/1722/test-cases/" + testCaseId);
-        $(".TestCaseLayout__name").shouldHave(text(testCaseName));
+       open("/project/1722/test-cases/" + testCaseId);
+       $(".TestCaseLayout__name").shouldHave(text(testCaseName));
+        $$(".TreeElement__node").get(0).shouldHave(text("Шажочек1"));
+        $$(".TreeElement__node").get(1).shouldHave(text("Шажочек2"));
+
     }
+
 }
